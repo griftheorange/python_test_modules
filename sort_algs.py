3160b9f31e2fb08b4c3e1dca73b4c00a252d001f
Griffin Poole <griffinpoole5@gmail.com>
2020-02-21 17:39:17 -0500

    bubble sort added

--- sort_algs.py
+++ sort_algs.py
@@ -2,20 +2,37 @@ a = [10, 9, 2024, 23452, -43, 2, 341]
 
 def selection_sort(arr):
     if arr[0]:
-        newArr = []
         for y in range(0, len(arr)-1):
-            min = arr[0]
-            index = 0
+            print(arr)
+            min = arr[y]
+            index = y
             for x in range(y, len(arr)):
                 if arr[x] < min:
                     min = arr[x]
                     index = x
-            s1 = slice(0, index)
-            s2 = slice(index+1, len(arr))
-            newArr.append(arr[index])
-            arr = arr[s1] + arr[s2]
-        return newArr
+            holder = arr[y]
+            arr[y] = min
+            arr[index] = holder
+        return arr
     else:
         return None
 
-meh
+def bubble_sort(arr):
+    has_swapped = True
+    while has_swapped:
+        has_swapped = False
+        for index in range(0, len(arr)-1):
+            if arr[index] > arr[index+1]:
+                has_swapped = True
+                holder = arr[index]
+                arr[index] = arr[index+1]
+                arr[index+1] = holder
+                print(arr)
+    return arr
+
+print(a)
+print(bubble_sort(a))
+print(a)
+
+# selection_sort(a)
+# bubble_sort(a)
