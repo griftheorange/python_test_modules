9d4d6e4211d581ff681e9b70be0832e9a47f2488
Griffin Poole <griffinpoole5@gmail.com>
2020-02-21 20:04:36 -0500

    recursive bubble sort and insertion sort added

--- sort_algs.py
+++ sort_algs.py
@@ -30,9 +30,40 @@ def bubble_sort(arr):
                 print(arr)
     return arr
 
-print(a)
-print(bubble_sort(a))
-print(a)
+def recursive_bubble_sort(arr):
+    print(arr)
+    for i, num in enumerate(arr):
+        # iterates through each index of array
+        try:
+            if arr[i+1] < num:
+                # on encounter of first 'unsorted' pair, swaps values then re-calls the function, starting back from beginning
+                arr[i] = arr[i+1]
+                arr[i+1] = num
+                recursive_bubble_sort(arr)
+        except IndexError:
+            # base case
+            # if the algorithm reaches the end of the array, arr[i+1] throws IndexError
+            # catches and passes, code reaches return statement
+            pass
+    return arr
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        print(arr)
+        value = arr[i]
+        j = i-1
+        while j >= 0 and value < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = value
+
+
+
+# print(a)
+# print(bubble_sort(a))
+# print(a)
 
 # selection_sort(a)
 # bubble_sort(a)
+# recursive_bubble_sort(a)
+insertion_sort(a)
