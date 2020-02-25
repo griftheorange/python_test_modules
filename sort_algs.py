7d869980480cb9408d8a8762adf1aed5eeba3f66
Griffin Poole <griffinpoole5@gmail.com>
2020-02-25 17:29:31 -0500

    Added merge sort

--- sort_algs.py
+++ sort_algs.py
@@ -73,6 +73,37 @@ def recursive_insertion_sort(arr, n):
     arr[j+1]=last
     print(arr)
 
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i+=1
+            else:
+                arr[k] = right[j]
+                j+=1
+            k+=1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j+=1
+            k+=1
+
+        print(arr)
 
 # print(a)
 # print(recursive_insertion_sort(a))
@@ -82,4 +113,5 @@ def recursive_insertion_sort(arr, n):
 # bubble_sort(a)
 # recursive_bubble_sort(a)
 # insertion_sort(a)
-recursive_insertion_sort(a, len(a))
+# recursive_insertion_sort(a, len(a))
+merge_sort(a)
