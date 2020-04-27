27429781531706fc6de434277a6e11beac306848
Griffin Poole <griffinpoole5@gmail.com>
2020-04-27 15:44:08 -0400

    Added ploting of graph data

--- data_analytics/data_analytics.py
+++ data_analytics/data_analytics.py
@@ -1,3 +1,11 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
 
-print("hello")
+budget_df = pd.read_excel("resources/budget_data.xlsx", usecols=['Date', 'Cost', 'Checking', 'Savings', 'Total'], parse_dates=['Date'])
+# budget_df.set_index('Date',inplace=True)
+budget_df.plot(x='Date')
+plt.show()
+print(budget_df)
+
+# pd.set_option("display.max_rows", None)
+# print(budget_df[['Date']])
