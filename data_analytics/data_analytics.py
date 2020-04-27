09193563fbba5d69ab07024e41efc5e45a5cbb21
Griffin Poole <griffinpoole5@gmail.com>
2020-04-27 15:54:04 -0400

    Switched out data file, formatted git

--- data_analytics/data_analytics.py
+++ data_analytics/data_analytics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-budget_df = pd.read_excel("resources/budget_data.xlsx", usecols=['Date', 'Cost', 'Checking', 'Savings', 'Total'], parse_dates=['Date'])
+budget_df = pd.read_excel("resources/data.xlsx", usecols=['Date', 'Cost', 'Checking', 'Savings', 'Total'], parse_dates=['Date'])
 # budget_df.set_index('Date',inplace=True)
 budget_df.plot(x='Date')
 plt.show()
