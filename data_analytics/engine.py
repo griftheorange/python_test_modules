426a76bfa16d34e59d490de309bc6ccc490f8282
Griffin Poole <griffinpoole5@gmail.com>
2020-04-27 18:46:58 -0400

    Reformatted file names, crashed OS with Tkinder, moving on to new GUI library

--- data_analytics/engine.py
+++ data_analytics/engine.py
@@ -5,9 +5,9 @@ from adapter import *
 def run(path):
     budget_df = load_excel_file(path+'/resources/data.xlsx')
     # plot_columns(budget_df, ColumnSets.BUDGET_ALL)
-    plot_columns(budget_df, ColumnSets.BUDGET_STD)
+    plot_budget(budget_df, ColumnSets.BUDGET_STD)
     save_as_pickle(budget_df, path+'/resources')
 
-def plot_columns(df, columns):
+def plot_budget(df, columns):
     df[columns].plot(x='Date')
     plt.show()
